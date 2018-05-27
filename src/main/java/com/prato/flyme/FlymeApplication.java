717af6c151334c488864c575107af549a8f68f2a
yunninglian <332510889@qq.com>
2018-05-28 05:22:27 +0800

    Initial commit

---
+++ src/main/java/com/prato/flyme/FlymeApplication.java
@@ -0,0 +1,12 @@
+package com.prato.flyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlymeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlymeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/prato/flyme/FlymeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prato.flyme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlymeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
